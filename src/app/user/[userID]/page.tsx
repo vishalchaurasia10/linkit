59e7b2b7631c69c5929e0b3fb4376d9a6d70b383
Cruz <lunac8@unlv.nevada.edu>
2023-07-01 16:33:30 -0400

    wip: adding prisma

--- src/app/user/[userID]/page.tsx
+++ src/app/user/[userID]/page.tsx
@@ -2,7 +2,9 @@ import { Metadata } from "next";
 import UserContainer from "@/app/components/userContainer";
 import UserAvatar from "@/app/components/userAvatar";
 import UserLink from "@/app/components/userLinks";
-
+import { prisma } from "../../../../backend/utils/prisma";
+import { User } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 interface PageProps {
   params: {
     userID: string;
@@ -16,6 +18,30 @@ export async function generateMetadata({
   };
 }
 const page = async ({ params }: PageProps) => {
+  try {
+    const user: User = await prisma.user.create({
+      data: {
+        email: "example@example.com",
+        name: "John Doe",
+        links: {
+          create: [
+            {
+              title: "Link 1",
+              linkURL: "https://example.com/link1",
+            },
+          ],
+        },
+      },
+    });
+
+    console.log("User created:", user);
+  } catch (error) {
+    console.error("Error writing to database: " + error);
+    if (error instanceof PrismaClientKnownRequestError) {
+      console.error(error.message);
+      console.error(error.meta);
+    }
+  }
   // this page receives the slug
   // fetch user data from db and create the link tree
   return (
